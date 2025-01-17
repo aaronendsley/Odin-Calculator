554234fdd43b5e1c93aae6bc19de45612e9cdb86
Aaron Endsley <aaronendsley@gmail.com>
2025-01-16 20:54:56 -0600

    Add Style to Calculator and write base functions

--- calculator.js
+++ calculator.js
@@ -1,1 +1,50 @@
 //This will contain the javascript for this project
+let base = 0;
+
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator){
+    let result;
+    switch(operator){
+        case '+':
+        result = add(num1, num2);
+        console.log('adding');
+        break;
+
+
+        case '-':
+        result = subtract(num1, num2);
+        console.log('subtracting');
+        break;
+
+        case '*':
+        result = multiply(num1, num2);
+        console.log('multiplying');
+        break;
+
+        case "/":
+        result = divide(num1, num2);
+        console.log('dividing', result);
+        break;
+
+        default:
+        console.log('no operation');
+    }
+
+    return result;
+}
